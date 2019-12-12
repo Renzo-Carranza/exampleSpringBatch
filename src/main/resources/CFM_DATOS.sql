8e76b9ab867209832bee7c5b1a3371c3c0a2c18c
renzo <renzo@TBS-CPU-080>
2019-12-12 16:31:00 -0500

    Se agrego el script de creacion de la tabla

---
+++ src/main/resources/CFM_DATOS.sql
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  renzo
+ * Created: 12/12/2019
+ */
+
+CREATE TABLE CFM_DATOS (
+"CFM_ID" NUMBER(15,0) PRIMARY KEY, 
+"CFM_TIPO" VARCHAR2(50), 
+"CFM_SUB_TIPO" VARCHAR2(50)
+);
+    
+CREATE SEQUENCE CFM_DATOS_SEQ MINVALUE 1 MAXVALUE 999999999 START WITH 1 INCREMENT BY 1 CACHE 20;
